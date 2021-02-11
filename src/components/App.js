2507f67c4915480774a65151a36a1e49f0e1b570
amanchimania <amanchimania@gmail.com>
2021-02-11 11:51:24 +0530

    testcases

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,73 @@
-import React, { Component, useState } from "react";
+import React, { useEffect, useState } from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
-    };
-
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
-		}
+function App(){
+    const [renderBall,setRenderBall]=useState(false);
+    const [posi,setPosi]=useState({left:5,top:5});
+    const [ballPosition,setBallPosition]=useState({left:"0",top:"0"});
+    const updateXY=(x,y)=>{
+        setBallPosition({
+            left:x,
+            top:y,
+        });
     }
+    const handleListener=(event)=>{
+        console.log(event.keyCode);
+        switch(event.keyCode){
+           case 39:
+               updateXY(ballPosition.left+5,ballPosition.top);
+               break;
+        
+        case 40 :
+               updateXY(ballPosition.left,ballPosition.top+5);
+               break;
+        
+        case 37:
+               updateXY(ballPosition.left-5,ballPosition.top);
+               break;
+        
+        case 38:
+               updateXY(ballPosition.left,ballPosition.top-5);
+               break;
+               default:
+                   break;
+        }
+    };
+    useEffect(()=>{
+        const keyListener=document.addEventListener("keydown",handleListener);
+        return function(){ document.removeEventListener("keydown",handleListener)};
+    },[ballPosition]);
+
+    const buttonClickHandler=()=> {
+        setRenderBall(true);
+    };
+    const renderBallOrButton=()=> {
+		if (renderBall) {
+
+		    return (
+            <div className="ball" 
+            style={{
+                left:ballPosition.left +"px",
+                top:ballPosition.top+"px",
+                position:"absolute",  
+            }}></div>
+            );
+		} else {
+		    return <button onClick={buttonClickHandler} >Click For One Ball</button>
+		}
+        
+    };
+return <div className="plaground">{renderBallOrButton()}</div>
+    
+   
+    
+};
+
+
+
+    
 
     // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
-
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
-}
-
+    
 
 export default App;
